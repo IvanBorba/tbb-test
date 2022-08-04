b27eb8117f904e2c25a75fc170f1bdc10dfb9660
Ivan Borba <mrborbaivan@gmail.com>
2022-08-04 17:26:34 -0300

    feat: created the ProductCard

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,54 +1,82 @@
 import Header from "../../components/Header";
 import bannerHeroMobile from "../../assets/images/banner-hero-mobile.png";
 import bannerHeroDesktop from "../../assets/images/banner-hero.png";
+import infoIcons from "../../assets/images/info-icons.png";
+import packBlue from "../../assets/images/pack-blue.png";
+import packGreen from "../../assets/images/pack-green.png";
+import packPink from "../../assets/images/pack-pink.png";
 import "./style.css";
+import ProductCard from "../../components/ProductCard";
+
+const mockedProducts = [
+  {
+    type: "wellness",
+    description: "intibiome wellness daily intimate wash",
+    image: packBlue,
+  },
+  {
+    type: "active",
+    description: "intibiome active extra protection intimate wash",
+    image: packGreen,
+  },
+  {
+    type: "agecare",
+    description: "intibiome agecare dryness relief intimate wash",
+    image: packPink,
+  },
+];
 
 const LandingPage = () => {
   const actualWidth = window.innerWidth;
   const isMobile = actualWidth < 769;
 
   return (
-    <div>
+    <>
       <Header isMobile={isMobile} />
-      <main>
+      <main className="main-container">
         <img
+          className="banner-hero"
           alt="Banner Hero, shows the logo, the product packs images, also some all-ages women with the following text: Cares for intimate microbiome to help strengthen natural intimate defence"
           src={isMobile ? bannerHeroMobile : bannerHeroDesktop}
         />
-        <section>
+        <section className="info-section-container">
           <h2>we're here to help</h2>
-          <p>
+          <p className="small-text">
             When it comes to caring for our most intimate areas, we've lost our
-            connection. <br /> Embarrassed, unwilling, or unable to communicate
-            with others, we're needlessly neglecting the parts of our bodies
-            that need it most. <br /> We're here to help. Providing you with the
-            expertise, knowledge and products you need to feel confident in your
-            personal care.
+            connection. <br /> <br /> Embarrassed, unwilling, or unable to
+            communicate with others, we're needlessly neglecting the parts of
+            our bodies that need it most. <br /> <br /> We're here to help.
+            Providing you with the expertise, knowledge and products you need to
+            feel confident in your personal care.
           </p>
-          <img></img>
-          <h2>
+          <img
+            className="info-icons-image"
+            alt="Image that show 3 icons with some legends, the first one is Promote, the second Preserve and the third is Protect."
+            src={infoIcons}
+          ></img>
+          <h2 className="large-heading">
             whatever your age. whatever your lifestyle. whatever your interests.
           </h2>
-          <p>
+          <p className="large-text">
             Co-created with gynaecologists, our revolutionary products have been
             expertly developed to support your intimate microbiome and pH
-            balance, and strengthen overall natural health. <br /> As the
+            balance, and strengthen overall natural health. <br /> <br /> As the
             experts in intimate hygiene, we want to bring discussion about
             intimate wellness care out of the dark and demystify the taboos that
             surround it.
           </p>
           <h2>our products</h2>
-          <div>
-            <div>Card</div>
-            <div>Card</div>
-            <div>Card</div>
+          <div className="products-list-container">
+            {mockedProducts.map((element, index) => (
+              <ProductCard product={element} key={index} />
+            ))}
           </div>
         </section>
       </main>
       <img></img>
       <section></section>
       <footer></footer>
-    </div>
+    </>
   );
 };
 
