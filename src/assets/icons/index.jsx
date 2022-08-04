a365438907118b9a43c578e574da1ce7cf47ad62
Ivan Borba <mrborbaivan@gmail.com>
2022-08-04 14:46:16 -0300

    feat: set the font and collors styles, also added the assets

---
+++ src/assets/icons/index.jsx
@@ -0,0 +1,94 @@
+export const MenuIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <path fill="none" d="M0 0H24V24H0z" data-name="Rectangle 27"></path>
+      <g
+        fill="#fff"
+        stroke="#707070"
+        strokeWidth="1"
+        data-name="Rectangle 56"
+        transform="translate(6 16)"
+      >
+        <rect width="12" height="2" stroke="none" rx="1"></rect>
+        <rect width="11" height="1" x="0.5" y="0.5" fill="none" rx="0.5"></rect>
+      </g>
+      <g
+        fill="#fff"
+        stroke="#707070"
+        strokeWidth="1"
+        data-name="Rectangle 57"
+        transform="translate(6 11)"
+      >
+        <rect width="12" height="2" stroke="none" rx="1"></rect>
+        <rect width="11" height="1" x="0.5" y="0.5" fill="none" rx="0.5"></rect>
+      </g>
+      <g
+        fill="#fff"
+        stroke="#707070"
+        strokeWidth="1"
+        data-name="Rectangle 58"
+        transform="translate(6 6)"
+      >
+        <rect width="12" height="2" stroke="none" rx="1"></rect>
+        <rect width="11" height="1" x="0.5" y="0.5" fill="none" rx="0.5"></rect>
+      </g>
+    </svg>
+  );
+};
+
+export const SearchIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <path fill="none" d="M0 0H24V24H0z" data-name="Rectangle 27"></path>
+      <path
+        fill="none"
+        stroke="#7b7070"
+        strokeLinecap="round"
+        strokeWidth="2"
+        d="M10.401 13.865l-3.884 3.822zm-1.4-3.365a4.5 4.5 0 014.5-4.5 4.5 4.5 0 014.5 4.5 4.5 4.5 0 01-4.5 4.5A4.5 4.5 0 019 10.5z"
+        data-name="Union 1"
+      ></path>
+    </svg>
+  );
+};
+
+export const InstagramIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="42.545"
+      height="42.581"
+      viewBox="0 0 42.545 42.581"
+    >
+      <g data-name="Layer 2" transform="translate(-.002)">
+        <g fill="#fff" data-name="Layer 1" transform="translate(.002)">
+          <path
+            d="M21.3 0c2.8.048 5.611.074 8.411.149a14.344 14.344 0 016.974 1.713 10.7 10.7 0 015.36 7.746 35.6 35.6 0 01.454 5.484c.064 4.139.052 8.278.006 12.418a24.463 24.463 0 01-.609 6.248 10.906 10.906 0 01-9.428 8.418 35.8 35.8 0 01-5.31.368c-3.908.052-7.819.045-11.725 0a28.951 28.951 0 01-6.171-.511A11.056 11.056 0 01.4 32.49a42.51 42.51 0 01-.363-5.263q-.077-5.933 0-11.866a28.589 28.589 0 01.512-6.079 11.049 11.049 0 019.51-8.844A51.78 51.78 0 0115.6.07c1.9-.061 3.8-.014 5.7-.014a.387.387 0 010-.056zM3.854 21.257H3.9c0 .736-.019 1.472 0 2.208.085 2.725.072 5.457.305 8.169a7.15 7.15 0 005.887 6.6 26.83 26.83 0 005.762.43q5.427.055 10.854 0c1.621-.018 3.248-.1 4.859-.276a7.24 7.24 0 006.633-5.912 26.834 26.834 0 00.429-5.762q.057-5.427 0-10.854a49.922 49.922 0 00-.276-4.86 7.251 7.251 0 00-5.923-6.622 26.583 26.583 0 00-5.716-.425 440.054 440.054 0 00-10.9 0 42.874 42.874 0 00-5.4.374 6.889 6.889 0 00-5.2 3.476 10.547 10.547 0 00-1.211 5.13c-.076 2.778-.103 5.556-.149 8.324z"
+            data-name="Path 31"
+            transform="translate(-.002)"
+          ></path>
+          <path
+            d="M134.087 123.532a10.953 10.953 0 11-10.93-10.952 10.991 10.991 0 0110.93 10.952zm-3.891-.018a7.064 7.064 0 10-7.033 7.084 7.1 7.1 0 007.033-7.084z"
+            data-name="Path 32"
+            transform="translate(-101.862 -102.224)"
+          ></path>
+          <path
+            d="M331.331 82.569a2.541 2.541 0 11-2.536-2.544 2.553 2.553 0 012.536 2.544z"
+            data-name="Path 33"
+            transform="translate(-296.241 -72.664)"
+          ></path>
+        </g>
+      </g>
+    </svg>
+  );
+};
