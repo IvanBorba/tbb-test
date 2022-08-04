703a37d2c1c23bb63190bee25a32485a93c2dd2c
Ivan Borba <mrborbaivan@gmail.com>
2022-08-04 16:13:48 -0300

    feat: almost finished the header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+import intibiomeLogo from "../../assets/images/logo-intibiome.png";
+import { MenuIcon, SearchIcon } from "../../assets/icons";
+import "./style.css";
+
+const Header = ({ isMobile }) => {
+  const [openMobileNavigation, setOpenMobileNavigation] = useState(false);
+
+  const handleMobileNavigation = () => {
+    setOpenMobileNavigation(!openMobileNavigation);
+    console.log(openMobileNavigation);
+  };
+
+  return (
+    <header className="header">
+      <div className="icons-container">
+        {isMobile ? (
+          openMobileNavigation ? (
+            <span
+              className="close-mobile-navigation-icon"
+              onClick={handleMobileNavigation}
+            >
+              X
+            </span>
+          ) : (
+            <span onClick={handleMobileNavigation}>
+              <MenuIcon />
+            </span>
+          )
+        ) : (
+          <span />
+        )}
+        <img alt="Main Logo" src={intibiomeLogo} />
+        <SearchIcon />
+      </div>
+      {isMobile && openMobileNavigation && (
+        <nav className="navigation-mobile-container">
+          <button className="navigation-mobile-button">brand philosophy</button>
+          <button className="navigation-mobile-button">
+            product technology
+          </button>
+          <button className="navigation-mobile-button">all products</button>
+          <button className="navigation-mobile-button">
+            intibiome wellness
+          </button>
+          <button className="navigation-mobile-button">intibiome active</button>
+          <button className="navigation-mobile-button">
+            intibiome agecare
+          </button>
+          <button className="navigation-mobile-button">article 1</button>
+          <button className="navigation-mobile-button">article 2</button>
+          <button className="navigation-mobile-button">article 3</button>
+          <button className="navigation-mobile-button">faq</button>
+          <button className="navigation-mobile-button">contact us</button>
+        </nav>
+      )}
+      {!isMobile && (
+        <nav className="navigation-desktop-container">
+          <button className="navigation-desktop-button">about us</button>
+          <button className="navigation-desktop-button">our products</button>
+          <button className="navigation-desktop-button">intimate health</button>
+          <button className="navigation-desktop-button">contact us</button>
+        </nav>
+      )}
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,18 +1,24 @@
+import Header from "../../components/Header";
+import bannerHeroMobile from "../../assets/images/banner-hero-mobile.png";
+import bannerHeroDesktop from "../../assets/images/banner-hero.png";
 import "./style.css";
 
 const LandingPage = () => {
+  const actualWidth = window.innerWidth;
+  const isMobile = actualWidth < 769;
+
   return (
     <div>
-      <header>
-        <div></div>
-        <nav></nav>
-      </header>
+      <Header isMobile={isMobile} />
       <main>
-        <img></img>
+        <img
+          alt="Banner Hero, shows the logo, the product packs images, also some all-ages women with the following text: Cares for intimate microbiome to help strengthen natural intimate defence"
+          src={isMobile ? bannerHeroMobile : bannerHeroDesktop}
+        />
         <section>
           <h2>we're here to help</h2>
           <p>
-            When it comes to caring for our most intimate areas, we’ve lost our
+            When it comes to caring for our most intimate areas, we've lost our
             connection. <br /> Embarrassed, unwilling, or unable to communicate
             with others, we're needlessly neglecting the parts of our bodies
             that need it most. <br /> We're here to help. Providing you with the
