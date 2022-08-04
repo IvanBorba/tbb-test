dd822271a8fdfb5e2324d446416fac6fa7bbb0fa
Ivan Borba <mrborbaivan@gmail.com>
2022-08-04 18:02:12 -0300

    feat: separated the mocked data from the page component

---
+++ src/mocks/index.jsx
@@ -0,0 +1,39 @@
+import packBlue from "../assets/images/pack-blue.png";
+import packGreen from "../assets/images/pack-green.png";
+import packPink from "../assets/images/pack-pink.png";
+import article1Image from "../assets/images/article1-image.png";
+import article2Image from "../assets/images/article2-image.png";
+import article3Image from "../assets/images/article3-image.png";
+
+export const mockedProducts = [
+  {
+    type: "wellness",
+    description: "intibiome wellness daily intimate wash",
+    image: packBlue,
+  },
+  {
+    type: "active",
+    description: "intibiome active extra protection intimate wash",
+    image: packGreen,
+  },
+  {
+    type: "agecare",
+    description: "intibiome agecare dryness relief intimate wash",
+    image: packPink,
+  },
+];
+
+export const mockedArticles = [
+  {
+    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: article1Image,
+  },
+  {
+    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: article2Image,
+  },
+  {
+    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: article3Image,
+  },
+];
--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -3,51 +3,14 @@ import bannerHeroDesktop from "../../assets/images/banner-hero.png";
 import bannerMidMobile from "../../assets/images/banner-mid-mobile.png";
 import bannerMidDesktop from "../../assets/images/banner-mid.png";
 import infoIcons from "../../assets/images/info-icons.png";
-import packBlue from "../../assets/images/pack-blue.png";
-import packGreen from "../../assets/images/pack-green.png";
-import packPink from "../../assets/images/pack-pink.png";
-import article1Image from "../../assets/images/article1-image.png";
-import article2Image from "../../assets/images/article2-image.png";
-import article3Image from "../../assets/images/article3-image.png";
 
 import ProductCard from "../../components/ProductCard";
+import ArticleCard from "../../components/ArticleCard";
 import Header from "../../components/Header";
 
+import { mockedArticles, mockedProducts } from "../../mocks";
+
 import "./style.css";
-import ArticleCard from "../../components/ArticleCard";
-
-const mockedProducts = [
-  {
-    type: "wellness",
-    description: "intibiome wellness daily intimate wash",
-    image: packBlue,
-  },
-  {
-    type: "active",
-    description: "intibiome active extra protection intimate wash",
-    image: packGreen,
-  },
-  {
-    type: "agecare",
-    description: "intibiome agecare dryness relief intimate wash",
-    image: packPink,
-  },
-];
-
-const mockedArticles = [
-  {
-    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image: article1Image,
-  },
-  {
-    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image: article2Image,
-  },
-  {
-    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image: article3Image,
-  },
-];
 
 const LandingPage = () => {
   const actualWidth = window.innerWidth;
